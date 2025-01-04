62284560308d38f0db3dd070e4c1c5c18df53238
Bernardo Zerda <bernardo.zerda@gmail.com>
2025-01-04 13:51:28 -0500

    Cóigo inicial, no muestra el formulario

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = "Formulario para datos básicos de un usuario";
+  user = new User();
+  
+  onSubmit() {
+    console.log(this.user);
+  }
+
+}
+
+export class User {
+  name: string = "";
+  email: string = "";
+}
