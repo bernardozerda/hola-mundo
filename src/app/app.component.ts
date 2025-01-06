c53e8f3a9da39f9a4c4a4f7cf4b64b5cacb105b7
Bernardo Zerda <bernardo.zerda@gmail.com>
2025-01-05 20:32:49 -0500

    Formuklario con un select que trae datos de un array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
 
-  title = 'Formulario del hola mundo';
+  title = 'Toma de pedidos';
+
+  clientes = [
+    { id: 0, nombre: 'Juan', apellido: 'Perez', edad: 25 },
+    { id: 1, nombre: 'Maria', apellido: 'Gomez', edad: 30 },
+    { id: 2, nombre: 'Pedro', apellido: 'Garcia', edad: 35 },
+    { id: 3, nombre: 'Laura', apellido: 'Fernandez', edad: 40 }
+  ];
 
   onSubmit() {
     alert('Formulario enviado');
