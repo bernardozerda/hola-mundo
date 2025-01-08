7619e266f103cb907b0798e61b1f7311fe9aa862
Bernardo Zerda <bernardo.zerda@gmail.com>
2025-01-07 20:55:48 -0500

    No recuerdo que hice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,20 @@ export class AppComponent {
   title = 'Toma de pedidos';
 
   clientes = [
-    { id: 0, nombre: 'Juan', apellido: 'Perez', edad: 25 },
-    { id: 1, nombre: 'Maria', apellido: 'Gomez', edad: 30 },
-    { id: 2, nombre: 'Pedro', apellido: 'Garcia', edad: 35 },
+    { id: 0, nombre: 'Juan',  apellido: 'Perez',     edad: 25 },
+    { id: 1, nombre: 'Maria', apellido: 'Gomez',     edad: 30 },
+    { id: 2, nombre: 'Pedro', apellido: 'Garcia',    edad: 35 },
     { id: 3, nombre: 'Laura', apellido: 'Fernandez', edad: 40 }
   ];
 
   pedidosForm = new FormGroup({ 
     vendedor: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
+    cliente: new FormControl('', Validators.required)
   });
 
-  onSubmit() {
-    alert('Formulario enviado = ' + this.pedidosForm.valid);
-  }
+  onSubmit() {  
+    alert(JSON.stringify(this.pedidosForm.value));
+  } 
 
 }
